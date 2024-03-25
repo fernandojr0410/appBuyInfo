077b4b4aadcf9b4bdbd98dd5ef0a9f6e56d91028
fernandojr0410 <fernando.junior300@hotmail.com>
2024-03-25 18:37:22 -0300

    estilizacao da tela de login e cadastro

---
+++ src/config/default.styles.ts
@@ -0,0 +1,17 @@
+import {StyleSheet } from "react-native";
+
+export const defaultStyles = StyleSheet.create({
+    container: {
+      flex: 1,
+      paddingLeft: 15,
+      paddingRight: 15,
+      paddingTop: 45,
+      paddingBottom: 80,
+    },
+    innerContainer: {
+      flex: 1,
+      justifyContent: "center",
+      alignItems: "center",
+      padding: 20,
+    },
+  });
--- src/pages/login/login.tsx
+++ src/pages/login/login.tsx
@@ -1,10 +1,140 @@
-import {View} from "react-native"
-import { Text } from "react-native-paper"
+import { useState } from "react";
+import { View, Dimensions } from "react-native";
+import { Avatar, Button, Text, TextInput } from "react-native-paper";
+import { defaultStyles } from "../../config/default.styles";
 
-export const Login = ({navigation}: any) => {
-    return(
-        <View>
-            <Text>Página Login</Text>            
+export const Login = ({ navigation }: any) => {
+  const { width, height } = Dimensions.get("window");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [isPasswordVisible, setIsPasswordVisible] = useState(false);
+  const [isRegisteringAccount, setIsregisteringAccount] = useState(false);
+  const [name, setName] = useState("");
+
+  // TODO: função botão criar conta deve validar se os campos de senha são iguais e tem mais de 6 digitos se sim, cria a conta, se não apresenta uma mensagem de erro
+
+  return (
+    <View style={defaultStyles.container}>
+      {/* Login */}
+      {!isRegisteringAccount ? (
+        <View style={defaultStyles.innerContainer}>
+          <View>
+            <Avatar.Image
+              size={84}
+              source={require("../../assets/avatar.png")}
+            />
+          </View>
+
+          <View>
+            <TextInput
+              label="Email"
+              value={email}
+              right={<TextInput.Icon icon="account" />}
+              style={{ marginTop: 16 }}
+              onChangeText={setEmail}
+            />
+
+            <TextInput
+              label="Senha"
+              value={password}
+              style={{ marginTop: 16 }}
+              secureTextEntry={!isPasswordVisible}
+              right={
+                <TextInput.Icon
+                  icon={isPasswordVisible ? "eye-off" : "eye"}
+                  onPress={() => setIsPasswordVisible(!isPasswordVisible)}
+                />
+              }
+              onChangeText={setPassword}
+            />
+
+            <Button
+              icon="account-key"
+              style={{ marginTop: 16 }}
+              mode="contained"
+              onPress={() => navigation.navigate("home")}
+            >
+              Login
+            </Button>
+            <Button
+              icon="account-plus"
+              style={{ marginTop: 16 }}
+              mode="contained-tonal"
+              onPress={() => setIsregisteringAccount(true)}
+            >
+              Cadastre-se
+            </Button>
+          </View>
         </View>
-    )
-}
+      ) : (
+        <View style={defaultStyles.innerContainer}>
+          <View>
+            <Avatar.Image
+              size={84}
+              source={require("../../assets/avatar.png")}
+            />
+          </View>
+          <View>
+            <TextInput
+              label="Nome"
+              value={name}
+              style={{ marginTop: 16 }}
+              right={<TextInput.Icon icon="account" />}
+              onChangeText={setName}
+            />
+            <TextInput
+              label="Email"
+              value={email}
+              right={<TextInput.Icon icon="email" />}
+              style={{ marginTop: 16 }}
+              onChangeText={setEmail}
+            />
+            <TextInput
+              label="Senha"
+              value={password}
+              style={{ marginTop: 16 }}
+              secureTextEntry={!isPasswordVisible}
+              right={
+                <TextInput.Icon
+                  icon={isPasswordVisible ? "eye-off" : "eye"}
+                  onPress={() => setIsPasswordVisible(!isPasswordVisible)}
+                />
+              }
+              onChangeText={setPassword}
+            />
+            <TextInput
+              label="Confirme a Senha"
+              value={password}
+              style={{ marginTop: 16 }}
+              secureTextEntry={!isPasswordVisible}
+              right={
+                <TextInput.Icon
+                  icon={isPasswordVisible ? "eye-off" : "eye"}
+                  onPress={() => setIsPasswordVisible(!isPasswordVisible)}
+                />
+              }
+              onChangeText={setPassword}
+            />
+            <Button
+              icon="account-plus"
+              style={{ marginTop: 16 }}
+              mode="contained"
+              onPress={() => navigation.navigate("home")}
+            >
+              Criar Conta
+            </Button>
+
+            <Button
+              icon="arrow-left"
+              style={{ marginTop: 16 }}
+              mode="contained-tonal"
+              onPress={() => setIsregisteringAccount(false)}
+            >
+              Voltar
+            </Button>
+          </View>
+        </View>
+      )}
+    </View>
+  );
+};
